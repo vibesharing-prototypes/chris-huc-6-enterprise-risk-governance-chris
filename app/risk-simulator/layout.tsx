b9eb03d2d34cbad398567ad6c2144b2d493ce268
Chris Avore <avore@erova.com>
2026-04-15 12:27:47 -0400

    Initial deploy of ERG v3 (HUC 6) from local source

--- app/risk-simulator/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { SimulationProvider } from "./SimulationContext";
-
-export default function RiskSimulatorLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <SimulationProvider>{children}</SimulationProvider>;
-}
